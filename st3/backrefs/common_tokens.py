aff522f58c5c19aa05cd81eb290c1cbe5a2fa19f
facelessuser <faceless.shop@gmail.com>
2017-09-26 21:33:28 -0600

    Upgrade to 2.0.2

--- st3/backrefs/common_tokens.py
+++ st3/backrefs/common_tokens.py
@@ -4,6 +4,7 @@ Common tokens shared between the different regex modules.
 Licensed under MIT
 Copyright (c) 2015 - 2016 Isaac Muse <isaacmuse@gmail.com>
 """
+import re
 
 # Unicode string related references
 utokens = {
@@ -24,7 +25,22 @@ utokens = {
     "nl": '\n',
     "negate": '^',
     "verbose_flag": 'x',
-    "unicode_flag": 'u'
+    "unicode_flag": 'u',
+    "group": "g",
+    "lc_bracket": "{",
+    "rc_bracket": "}",
+    "group_start": r"\g<",
+    "group_end": ">",
+    "format_replace_group": re.compile(
+        r'(\{{2}|\}{2})|(\{(?:[a-zA-Z]+[a-zA-Z\d_]*|0*(?:[1-9][0-9]?)?)?(?:\[[^\]]+\])?\})'
+    ),
+    "minus": "-",
+    "binary": "b",
+    "octal": "o",
+    "hex": "x",
+    "zero": "0",
+    "unicode_narrow": "u",
+    "unicode_wide": "U"
 }
 
 # Byte string related references
@@ -48,5 +64,20 @@ btokens = {
     "nl": b'\n',
     "negate": b'^',
     "verbose_flag": b'x',
-    "unicode_flag": b'u'
+    "unicode_flag": b'u',
+    "group": b"g",
+    "lc_bracket": b"{",
+    "rc_bracket": b"}",
+    "group_start": br"\g<",
+    "group_end": b">",
+    "format_replace_group": re.compile(
+        br'(\{{2}|\}{2})|(\{(?:[a-zA-Z]+[a-zA-Z\d_]*|0*(?:[1-9][0-9]?)?)?(?:\[[^\]]+\])?\})'
+    ),
+    "minus": b"-",
+    "binary": b"b",
+    "octal": b"o",
+    "hex": b"x",
+    "zero": b"0",
+    "unicode_narrow": b"u",
+    "unicode_wide": b"U"
 }
