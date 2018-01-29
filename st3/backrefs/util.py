702fc7f7ae6a7a505e45b8e2d722ca170be9330d
facelessuser <faceless.shop@gmail.com>
2018-01-28 19:44:51 -0700

    Upgrade to 3.0.3

--- st3/backrefs/util.py
+++ st3/backrefs/util.py
@@ -13,40 +13,60 @@ PY36 = (3, 6) <= sys.version_info
 PY37 = (3, 7) <= sys.version_info
 
 if PY3:
-    from functools import lru_cache  # noqa: F401
+    from functools import lru_cache  # noqa F401
+    import copyreg  # noqa F401
 
     string_type = str
     binary_type = bytes
     unichar = chr
 
-    class Tokens(object):
-        """Tokens base for Python 3."""
-
-        def iternext(self):
-            """Common override method."""
-
-        def __next__(self):
-            """Python 3 iterator compatible next."""
-
-            return self.iternext()
-
 else:
-    from backports.functools_lru_cache import lru_cache  # noqa: F401
+    from backports.functools_lru_cache import lru_cache  # noqa F401
+    import copy_reg as copyreg  # noqa F401
 
     string_type = unicode  # noqa F821
     binary_type = str  # noqa F821
     unichar = unichr  # noqa F821
 
-    class Tokens(object):
-        """Tokens base for Python 2."""
 
-        def iternext(self):
-            """Common override method."""
+class StringIter(object):
+    """Preprocess replace tokens."""
 
-        def next(self):
-            """Python 2 iterator compatible next."""
+    def __init__(self, string):
+        """Initialize."""
 
-            return self.iternext()
+        self.string = string
+        self.max_index = len(string) - 1
+        self.index = 0
+
+    def __iter__(self):
+        """Iterate."""
+
+        return self
+
+    def __next__(self):
+        """Python 3 iterator compatible next."""
+
+        return self.iternext()
+
+    def rewind(self, count):
+        """Rewind index."""
+
+        self.index -= count
+
+    def iternext(self):
+        """Iterate through characters of the string."""
+
+        if self.index > self.max_index:
+            raise StopIteration
+
+        char = self.string[self.index]
+        self.index += 1
+
+        return char
+
+    # Python 2 iterator compatible next.
+    next = __next__  # noqa A002
 
 
 def uchr(i):
